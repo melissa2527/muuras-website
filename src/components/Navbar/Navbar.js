1dc4c7744ac9cb33bdac8f7c24b37db2c5779d6d
annie2527 <you@example.com>
2021-03-19 22:50:38 +0100

    navbar css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import styles from './Navbar.module.css';
-// import {Link} from 'react-router-dom';
+import navStyles from './Navbar.module.css';
+import Link from 'next/link';
 
 const NavBar = () => {
     const [clicked, setClicked] = useState(false);
@@ -25,33 +25,33 @@ const NavBar = () => {
 
     return (
         <>
-            <nav className={styles.container}>
-                <div className={styles.nav}>
-                    <Link to='/' className='navbar-logo' onClick={closeMenuMobile}>
-                        <img src='img/muuras.png' alt='' height={50}/>
+            <nav className={navStyles.nav}>
+                <div className={navStyles.container}>
+                    <Link href='/' className={navStyles.navLogo} onClick={closeMenuMobile}>
+                        <img src='img/muuras.png' alt='Muuras' height={50}/>
                     </Link>
-                    <div className='menu-icon' onClick={handleClick}>
+                    <div className={navStyles.menu} onClick={handleClick}>
                         <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}/>
                     </div>
                     <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-link' onClick={closeMenuMobile}>
+                        <li className={navStyles.navItem}>
+                            <Link href='/' className={navStyles.link} onClick={closeMenuMobile}>
                                 Home
                             </Link>
                         </li>
-                        <li className='nav-item'>
-                            <Link to='/what-we-do' className='nav-link' onClick={closeMenuMobile}>
+                        <li className={navStyles.navItem}>
+                            <Link href='/what-we-do' className={navStyles.link} onClick={closeMenuMobile}>
                                 What We Do
                             </Link>
                         </li>
-                        <li className='nav-item'>
-                            <Link to='/team' className='nav-link' onClick={closeMenuMobile}>
+                        <li className={navStyles.navItem}>
+                            <Link href='/team' className={navStyles.link} onClick={closeMenuMobile}>
                                 Our Team
                             </Link>
                         </li>
                     </ul>
                     {button ? <button>
-                        <Link to='/contact' className='contact-btn'>
+                        <Link href='/contact' className={navStyles.contactBtn}>
                             Contact
                         </Link>
                     </button> : null}
